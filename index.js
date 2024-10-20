ea932a54ff6069677e81dd7f9a5b0712e5562692
StanovskaSofia bc2024-3 <stanovska.sofia@gmail.com>
2024-10-20 17:09:56 +0300

    1 частина

---
+++ index.js
@@ -0,0 +1,35 @@
+const { program } = require('commander');
+const fs = require('fs');
+
+program
+  .option('-i, --input <path>', 'input file path')
+  .option('-o, --output <path>', 'output file path')
+  .option('-d, --display', 'display result in console');
+
+program.parse(process.argv);
+
+const options = program.opts();
+
+if (!options.input) {
+  console.error("Please, specify input file");
+  process.exit(1); 
+}
+
+if (!fs.existsSync(options.input)) {
+  console.error("Cannot find input file");
+  process.exit(1); 
+}
+
+const data = fs.readFileSync(options.input, 'utf-8');
+
+if (!options.output && !options.display) {
+  process.exit(0);
+}
+
+if (options.display) {
+  console.log(data);
+}
+
+if (options.output) {
+  fs.writeFileSync(options.output, data); 
+}
